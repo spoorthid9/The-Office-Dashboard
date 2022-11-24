c7d987b943086a1ee6134b97b99bda2d6450a2f6
spoorthid9 <118953638+spoorthid9@users.noreply.github.com>
2022-11-24 10:56:49 +0530

    Update director.sql

--- director.sql
+++ director.sql
@@ -1,6 +1,9 @@
-##Number of episodes directed by the cast members
+#Number of episodes directed by the cast members
+
 SELECT EpisodeTitle, Director
 FROM proj1.the_office_series
 WHERE Director
 in('John Krasinski', 'Steve Carell', 'Paul Lieberstein', 'Rainn Wilson', 'B.J. Novak')
 ORDER BY Director
+
+![directorpic](https://user-images.githubusercontent.com/118953638/203701229-2830347b-8fde-4d6b-9aad-192cf487ddd5.jpg)
