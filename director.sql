202773cafabd661eb3ad64959d37105dee1b4f0f
spoorthid9 <118953638+spoorthid9@users.noreply.github.com>
2022-11-24 11:01:47 +0530

    Update director.sql

--- director.sql
+++ director.sql
@@ -6,4 +6,4 @@ WHERE Director
 in('John Krasinski', 'Steve Carell', 'Paul Lieberstein', 'Rainn Wilson', 'B.J. Novak')
 ORDER BY Director
 
-![directorpic](https://user-images.githubusercontent.com/118953638/203701229-2830347b-8fde-4d6b-9aad-192cf487ddd5.jpg)
+
